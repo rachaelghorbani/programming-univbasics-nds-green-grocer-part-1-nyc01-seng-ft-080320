db219e18de1f6920b2b495bbcef588bd74c6d913
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-27 17:47:27 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,13 +17,12 @@ end
 def consolidate_cart(cart)
 
   new_cart = []
-
   counter = 0 
   while counter < cart.length 
     name = cart[counter][:item]
     new_cart_item = find_item_by_name_in_collection(name, new_cart)
     if new_cart_item  
-       new_cart_item[:count] += 1
+      new_cart_item[:count] += 1
     else
       new_cart_item = {
         :item => name,
@@ -35,6 +34,31 @@ def consolidate_cart(cart)
     end
     counter += 1
   end    
- new_cart
+new_cart
 end 
+
+######### USING ENUMERABLES #########
+
+def find_item_by_name_in_collection(item_name, cart)
+  cart.each do |item_hash|
+    if item_hash[:item] == item_name.upcase
+      return item_hash
+    end
+  end
+  nil
+end
+
+def consolidate_cart(unconsolidated_cart)
+  consolo_cart = []
+  unconsolidated_cart.each do |item_hash| 
+    item_name = item_hash[:item]
+    new_cart_item = find_item_by_name_in_collection(item_name, consolo_cart)
+  if new_cart_item
+    new_cart_item[:count] += 1
+  else 
+    consolo_cart.push(item_hash)
+    item_hash[:count] = 1
+  end
+ end
+end
   
